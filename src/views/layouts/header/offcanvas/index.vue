4694a5c601df3c3548b2d4ce6267855a64de6f4f
Leofer Dela Cruz <Leoferma099@gmail.com>
2025-04-02 19:17:32 +0800

    modified:   src/views/layouts/header/offcanvas/index.vue

--- src/views/layouts/header/offcanvas/index.vue
+++ src/views/layouts/header/offcanvas/index.vue
@@ -9,6 +9,7 @@
             <div
                 v-if="!itemsLoading">
                 <div v-if="items.length > 0">
+
                     <item-component
                     class="notification"
                         v-for="(item, index) in items"
@@ -47,7 +48,9 @@ export default
             items: [],
             itemsLoading: false,
             isEmpty: true,
+
             count: 0,
+
             selectedItem: {},
         };
     },
@@ -55,13 +58,8 @@ export default
     created()
     {
         this.fetchTicketNotifications();
-        this.startAutoRefresh();
-        console.log(this, 'OFFCANVAS');
-    },
 
-    beforeUnmount()
-    {
-        clearInterval(this.refreshInterval);
+        console.log(this, 'OFFCANVAS');
     },
     
     methods:
@@ -85,13 +83,6 @@ export default
             {
                 this.items = response.data.data;
             }
-        },
-
-        startAutoRefresh()
-        {
-            this.refreshInterval = setInterval(() => {
-                this.fetchTicketNotifications();
-            }, 5000); // Refresh every 5 seconds
         },
 
         selectItem(item)
